d1413669d13227c4f03d677badfbbf9259f22650
Chandradev819 <chandradev819@gmail.com>
2025-09-17 17:12:39 +0530

    Code updated.

---
+++ Emp.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp28
+{
+    [ValidatableType]
+    public class Emp 
+    {
+        [Required(ErrorMessage = "First name is required.")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        public string? LastName { get; set; }
+
+        public Address Address { get; set; } = new();
+
+    }
+    public class Address
+    {
+        [Required(ErrorMessage = "Street is required.")]
+        public string? Street { get; set; }
+
+        [Required(ErrorMessage = "City is required.")]
+        public string? City { get; set; }
+    }
+}
